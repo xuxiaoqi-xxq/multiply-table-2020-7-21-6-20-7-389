8390c2cd3d8cc982e3f4892f0aa466aa3d6b558e
xxq <xxq_work@163.com>
2020-07-21 20:53:48 +0800

    [multiple table][feature]

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
 function createMultiplyTable(start, end) {
-  return null;
+    return isNumbersValid(start, end) ? generateMultiplyTable(start, end) : null;
+}
+
+function isNumbersValid(start, end) {
+    if (end < start || start < 1 || end > 1000) {
+        return false;
+    }
+    return true;
+}
+
+function generateMultiplyTable(start, end) {
+    let table = '';
+    for (let i = start; i <= end; i++) {
+        for (let j = start; j <= i; j++) {
+            table += j + '*' + i + '=' + i * j;
+            if (j != i) {
+                table += '\t';
+            }
+        }
+        if (i != end) {
+            table += '\n';
+        }
+    }
+    return table;
 }
 
 module.exports = {
-  createMultiplyTable,
+    createMultiplyTable,
 };
---
+++ pdca.md
@@ -0,0 +1,23 @@
+###### createMultiplyTable
+- Plan: 1 min
+- Do: 1 min
+- Check: There is a clear structure 'cause already written those code once.
+- Act: pratice makes more perfect.
+
+###### isNumbersValid
+- Plan: 1 min
+- Do: 1 min
+- Check: There is a clear structure 'cause already written those code once.
+- Act: pratice makes more perfect.
+
+###### generateMultiplyTable
+- Plan: 3 min
+- Do: 2 min
+- Check: In class praticing, I've made two mistakes here. Cause them, I am impressive in those codes.
+- Act: Mistakes is a good way to understand what I've learned.
+
+###### pass the test
+- Plan: 1 time
+- Do: 2 times
+- Check: Mistake function name.
+- Act: Be careful all the time! Don't sacrifice more time for hurry.
